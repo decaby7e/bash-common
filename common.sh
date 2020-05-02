b686d7c824a789b48d3b6e009f3cc2ac2cb52ff0
decaby7e <decaby7e@ranvier.net>
2020-05-02 15:28:03 -0400

    Update common.sh

--- common.sh
+++ common.sh
@@ -37,57 +37,30 @@ SCRIPT_PATH=$(dirname $(realpath -s $0))
 
 ## Logging Variables ##
 
-#
-# For use when debugging scripts. Should be removed
-# in production scripts...
-#
-# Usage: debug "Debug Message"
-#
+# Usage: debug 'Debug Message'
 debug(){
-  printf '\033[0;33m'[ DEBUG ]'\033[0m' $1\n"
+  printf "'\033[0;33m'Debug'\033[0m' $1\n"
 }
 
-#
-# For use with any kind of common info to be
-# displayed to the terminal. Should allow for
-# sparse use of printf and echo in giving the
-# user information.
-#
-# Usage: info "Information Message"
-#
+# Usage: info 'Information Message'
 info(){
-  printf "'\033[1;34m'[ INFO ]'\033[0m' $1\n"
+  printf "'\033[1;34m'Info'\033[0m' $1\n"
 }
 
-#
-# Warn the user of a serious error but one
-# that is not serious enough to cause a crash
-#
-# Usage: warn "Warning Message"
-#
+# Usage: warn 'Warning Message'
 warn(){
-  printf "'\033[1;33m'[WARNING]'\033[0m' $1\n"
+  printf "'\033[1;33m'Warn'\033[0m' $1\n"
 }
 
-#
-# Warn the user of a script-breaking crash
-# then immediatly exit
-#
-# Usage: fatal "Error Message"
-#
+# Usage: fatal 'Error Message'
 fatal(){
-  printf "'\033[0;31m'[ FATAL ] $1\n"
+  printf "'\033[0;31m'Fatal $1\n"
   exit 1
 }
 
 ## Checks ##
 
-#
-# Will check if user is root. If not, it will throw
-# a fatal error and notify the user to use root.
-#
 # Usage: root_check
-#
 root_check(){
   if [ "$EUID" -ne 0 ]; then
     fatal "Must be run as root."
@@ -95,15 +68,10 @@ root_check(){
 }
 
 #
-# Will lock a script to prevent several instances running
-# at the same time.
-# 
-# Alternative in one line from http://man7.org/linux/man-pages/man1/flock.1.html
-#
-# [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
-#
 # Usage: lock_self
-# 
+#
+# Alternative in one line from http://man7.org/linux/man-pages/man1/flock.1.html
+# [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
 #
 lock_self(){
   LOCK_FILE=a.lock
